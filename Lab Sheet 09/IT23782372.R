4a2c7389ef9b5f393d8970923fc3d6ff3c3d3d2e
it23782372 <it23782372@my.sliit.lk>
2025-10-01 21:49:35 +0530

    Add files via upload

---
+++ Lab Sheet 09/IT23782372.R
@@ -0,0 +1,9 @@
+setwd("C://Users//HP//Desktop//IT23782372")
+getwd()
+#1
+set.seed(123)
+bake_time<-rnorm(25,mean=45,sd=2)
+print(bake_time)
+#ii
+t.test(bake_time,mu=46,alternative = "less",conf.level = 0.95)
+print(t.test)
